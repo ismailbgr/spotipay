75866c46fcdaf2bb49517c6f40e7fb49947a2973
ismailbgr <ozismailbugra@gmail.com>
2018-06-15 10:13:43 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'SpotiPAY',
+  // App id
+  id: 'com.ismailbgr.spotipay',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+
+    {
+      name: 'about',
+      path: '/about/',
+      url: 'about.html',
+    },
+    
+    {
+      name: 'main',
+      path: '/main/',
+      url: 'main.html',
+    },
+    {
+      name: 'admin',
+      path: '/admin/',
+      url: 'admin.html',
+    },
+  
+  
+  ],
+  
+    statusbar: {
+    iosOverlaysWebview: true,
+  },
+
+ methods: {
+    alert: function() {
+      app.dialog.alert('Hello World');
+    }
+  },
+
+  panel: {
+    swipe: 'left',
+  }
+
+});
+
+          var mainView = app.views.create('.view-main');
